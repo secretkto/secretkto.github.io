f91724f86e21d69f4a90a95a9920d0376df213d7
Artiom <artiomponomariovcode@gmail.com>
2021-03-02 07:34:46 +0200

    markup finished

--- development/js/index.js
+++ development/js/index.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
   inlineSVG.init({
-     svgSelector: '.svg--inline',
+    svgSelector: '.svg--inline',
   }, function () {
     console.log('All svg inlined');
   });
+
+  let $faqItem = $('.faq .box--item');
+  let $faqButton = $('.faq .button');
+  $faqButton.click(function () {
+    $faqItem.each(function () {
+      $(this).addClass('isActive')
+    })
+  })
+  $faqItem.click(function () {
+    $(this).toggleClass('isActive')
+  })
 });
 
