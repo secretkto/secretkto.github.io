3b03ef37ae3a6810dd866a3e263f0c25280ef9d5
Artiom <artiomponomariovcode@gmail.com>
2021-03-10 02:46:17 +0200

    menu added

--- development/js/index.js
+++ development/js/index.js
@@ -7,14 +7,25 @@ $(document).ready(function () {
 
   let $videoHorizontalPlaceholder = $('.video--placeholder');
   let $videoHorizontal = $('.video--horizontal');
+  let $menu = $('.menu');
+  let $menuOpen = $('.img--hamburger');
+  let $menuClose = $('.menu .img--close');
   let $modalOpen = $('.button--modal');
   let $modalClose = $('.modal .img--close');
   let $modal = $('.modal');
   let $faqItem = $('.faq .box--item');
   let $faqButton = $('.faq .button');
+  let $header = $('.section--header');
+
   $videoHorizontalPlaceholder.click(function () {
     $(this).removeClass('isActive');
     $videoHorizontal.addClass('isActive');
+  });
+  $menuOpen.click(function () {
+    $menu.toggleClass('isActive')
+  });
+  $menuClose.click(function () {
+    $menu.removeClass('isActive')
   });
   $modalClose.click(function () {
     $modal.removeClass('isActive')
@@ -56,6 +67,15 @@ $(document).ready(function () {
     },
     onInit: function (slick) {
       $(slick.$slides.get(0)).addClass('isActive');
+    }
+  });
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    console.log(scroll)
+    if (scroll >= 500) {
+      $header.addClass("isFixed");
+    } else {
+      $header.removeClass("isFixed");
     }
   });
 });
