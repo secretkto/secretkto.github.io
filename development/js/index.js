d3972ef298ff059b989d7045835c86082404b151
Artiom <artiomponomariovcode@gmail.com>
2021-03-17 07:33:29 +0200

    slider fixes

--- development/js/index.js
+++ development/js/index.js
@@ -1,13 +1,6 @@
 $(document).ready(function () {
-  inlineSVG.init({
-    svgSelector: '.svg--inline',
-  }, function () {
-    console.log('All svg inlined');
-  });
-
   let $videoHorizontalPlaceholder = $('.video--placeholder');
   let $videoHorizontal = $('.video--horizontal');
-  let $menu = $('.menu');
   let $menuOpen = $('.img--hamburger');
   let $menuClose = $('.menu .img--close');
   let $modalOpen = $('.button--modal');
@@ -32,7 +25,31 @@ $(document).ready(function () {
     $modal.removeClass('isActive')
   });
   $modalOpen.click(function () {
-    $modal.addClass('isActive')
+    $modal.addClass('isActive');
+    $body.addClass('isModalActive');
+    $('.slider--main').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: true,
+      arrows: true,
+      fade: true,
+      asNavFor: '.slider--nav',
+      prevArrow: '<div class="icon icon--left"><img src="./img/icon--arrow-left.svg" class="img img--arrow" alt="icon arrow"></div>',
+      nextArrow: '<div class="icon icon--right"><img src="./img/icon--arrow-right.svg" class="img img--arrow" alt="icon arrow"></div>',
+    });
+    $('.slider--nav').slick({
+      slidesToShow: 4,
+      asNavFor: '.slider--main',
+      focusOnSelect: true,
+      arrows: false,
+      onAfterChange: function (slide, index) {
+        $('.isActive').removeClass('isActive');
+        $(this.$slides.get(index)).addClass('isActive');
+      },
+      onInit: function (slick) {
+        $(slick.$slides.get(0)).addClass('isActive');
+      }
+    });
   });
   $faqButton.click(function () {
     $faqItem.each(function () {
@@ -41,35 +58,81 @@ $(document).ready(function () {
   });
   $faqItem.click(function () {
     $(this).toggleClass('isActive')
-  })
-  $('.slider--main').slick({
-    slidesToShow: 1,
+  });
+
+  $('.slider--cards.is1').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
     draggable: true,
     arrows: true,
-    fade: true,
-    asNavFor: '.slider--nav',
+    dots: false,
     prevArrow: '<div class="icon icon--left"><img src="./img/icon--arrow-left.svg" class="img img--arrow" alt="icon arrow"></div>',
     nextArrow: '<div class="icon icon--right"><img src="./img/icon--arrow-right.svg" class="img img--arrow" alt="icon arrow"></div>',
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '120px',
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true
+        }
+      },
+    ]
   });
-  $('.slider--nav').slick({
-    slidesToShow: 4,
-    asNavFor: '.slider--main',
-    focusOnSelect: true,
-    arrows: false,
-    onAfterChange: function (slide, index) {
-      console.log("slider-nav change");
-      console.log(this.$slides.get(index));
-      $('.isActive').removeClass('isActive');
-      $(this.$slides.get(index)).addClass('isActive');
-    },
-    onInit: function (slick) {
-      $(slick.$slides.get(0)).addClass('isActive');
-    }
+  $('.slider--cards.is2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: true,
+    dots: false,
+    prevArrow: '<div class="icon icon--left"><img src="./img/icon--arrow-left.svg" class="img img--arrow" alt="icon arrow"></div>',
+    nextArrow: '<div class="icon icon--right"><img src="./img/icon--arrow-right.svg" class="img img--arrow" alt="icon arrow"></div>',
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '120px',
+          centerMode: false,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true
+        }
+      },
+    ]
   });
   $(window).scroll(function () {
     let scroll = $(window).scrollTop();
-    console.log(scroll)
     if (scroll >= 500) {
       $header.addClass("isFixed");
     } else {
